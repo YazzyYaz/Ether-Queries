81113616a6a3021667c88f927d7b00d28a4069ff
Yaz Khoury <yaz.khoury@gmail.com>
2019-01-14 15:58:34 -0500

    feat: Get working query for hashrate

--- hashrate.sql
+++ hashrate.sql
@@ -1,8 +1,9 @@
 WITH block_rows AS (
-  SELECT *, ROW_NUMBER() OVER (ORDER BY timestamp) AS rn 
+  SELECT *, ROW_NUMBER() OVER (ORDER BY timestamp) AS rn
   FROM `bigquery-public-data.ethereum_blockchain.blocks`
 )
-SELECT TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND)
+SELECT mp.timestamp as block_time, TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as time_elapsed, mp.difficulty / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as hashrate
 FROM block_rows mc
 JOIN block_rows mp
 ON  mc.rn = mp.rn - 1
+ORDER BY block_time asc
