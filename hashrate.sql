e216e36eca3f8e36e6542e3ba87c28ea36c67413
Yaz Khoury <yaz.khoury@gmail.com>
2019-01-17 17:45:13 -0500

    feat: update hashrate query

--- hashrate.sql
+++ hashrate.sql
@@ -1,8 +1,11 @@
 WITH block_rows AS (
   SELECT *, ROW_NUMBER() OVER (ORDER BY timestamp) AS rn
-  FROM `bigquery-public-data.ethereum_blockchain.blocks`
+  FROM `crypto-etl-ethereum-dev.ethereum_classic_blockchain.blocks`
 )
-SELECT mp.timestamp as block_time, TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as time_elapsed, mp.difficulty / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as hashrate
+SELECT mp.timestamp as block_time, 
+TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as time_elapsed,
+((mp.difficulty + mc.difficulty) / 2) as average_difficulty,
+((mp.difficulty + mc.difficulty) / 2) / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as hashrate
 FROM block_rows mc
 JOIN block_rows mp
 ON  mc.rn = mp.rn - 1
