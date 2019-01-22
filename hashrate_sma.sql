1fc1eee7ab6c9a0b13349183ef950d2910839fb3
Yaz Khoury <yaz.khoury@gmail.com>
2019-01-22 15:57:52 -0500

    style: Update sql syntax

--- hashrate_sma.sql
+++ hashrate_sma.sql
@@ -4,11 +4,11 @@ WITH block_rows AS (
 ),
 time_and_hash AS (
   SELECT
-  mp.timestamp as block_time,
-  TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as delta_block_time,
-  ((mp.difficulty + mc.difficulty) / 2) as average_difficulty,
-  ((mp.difficulty + mc.difficulty) / 2) / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as hashrate,
-  1 / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as block_frequency
+  mp.timestamp AS block_time,
+  TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) AS delta_block_time,
+  ((mp.difficulty + mc.difficulty) / 2) AS average_difficulty,
+  ((mp.difficulty + mc.difficulty) / 2) / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) AS hashrate,
+  1 / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) AS block_frequency
   FROM block_rows mc
   JOIN block_rows mp
   ON mc.rn = mp.rn - 1
