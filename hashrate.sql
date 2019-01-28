a5ff4c2e30deacb8c922af73f99c0e8381a25bf7
Yaz Khoury <yaz.khoury@gmail.com>
2019-01-28 16:43:54 -0500

    style: Update hashrate query

--- hashrate.sql
+++ hashrate.sql
@@ -2,11 +2,11 @@ WITH block_rows AS (
   SELECT *, ROW_NUMBER() OVER (ORDER BY timestamp) AS rn
   FROM `crypto-etl-ethereum-dev.ethereum_classic_blockchain.blocks`
 )
-SELECT mp.timestamp as block_time, 
-TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as time_elapsed,
-((mp.difficulty + mc.difficulty) / 2) as average_difficulty,
-((mp.difficulty + mc.difficulty) / 2) / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) as hashrate
+SELECT mp.timestamp AS block_time, 
+TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) AS time_elapsed,
+((mp.difficulty + mc.difficulty) / 2) AS average_difficulty,
+((mp.difficulty + mc.difficulty) / 2) / TIMESTAMP_DIFF(mp.timestamp, mc.timestamp, SECOND) AS hashrate
 FROM block_rows mc
 JOIN block_rows mp
 ON  mc.rn = mp.rn - 1
-ORDER BY block_time asc
+ORDER BY block_time ASC
